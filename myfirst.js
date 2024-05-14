7225585c4a241f9b07b7d1810049be18e2249146
bencame <bencame@iinet.net.au>
2024-05-14 14:04:06 +0100

    using pg pools, handle different table requests

--- myfirst.js
+++ myfirst.js
@@ -3,16 +3,22 @@ var pg = require('pg')
 var http = require('http');
 const { error } = require('console');
 const CONTENT_TYPE_JSON = { "Content-Type": "application/json" };
-const { Client } = pg
+const { Pool } = pg
 
+const pool = new Pool ({
+    host: 'localhost', 
+    port: 5432,
+    username: 'polygot',
+    database: 'babel'
+})
 
 const bensServer = http.createServer((request, res) => {
     const { headers, method, url } = request;
 
     if (method === 'GET'){
         handelGetRequests(url).then(response => {
-            // response.setHeader('200',CONTENT_TYPE_JSON)
             console.log("this is my response   " + response)
+            console.log(JSON.stringify(response))
             res.write(JSON.stringify(response))
             res.end()
         })
@@ -28,11 +34,19 @@ bensServer.listen(8080, ()=>console.log("listening on 8080"))
 
 
 async function handelGetRequests (parsedURL) {
+    //TODO this is working in the current form, but how can i dynamically add a table from the application
     if (parsedURL === "/japanese"){
         console.log("successgully passed handel request")
         temp = await getTable("japanese")
         return temp.rows[0]
-        // console.log("yoyoyo this got called" + temp)
+    } else if(parsedURL === "/svenska"){
+        console.log("successgully passed handel request")
+        temp = await getTable("svenska")
+        return temp.rows[0]
+    } else if(parsedURL === "/espanol"){
+        console.log("successgully passed handel request")
+        temp = await getTable("espanol")
+        return temp.rows[0]
     } else {
         console.log("nothing to see here!!")
     }
@@ -40,27 +54,12 @@ async function handelGetRequests (parsedURL) {
 
 async function getTable(table){
     console.log("get table")
-    const client = new Client ({ //TODO need to create a client each time i call this function, not cleanest method... will refactor
-        host: 'localhost', 
-        port: 5432,
-        username: 'polygot',
-        database: 'babel'
-    })
-    client.connect()//TODO im sure i can refactor this so i dont need to open a client to the postgres server everytime
+    client = await pool.connect()
     var ans = await client.query('SELECT * FROM ' + table)
-    client.end()
+    client.release()
     console.log(ans.rows[0])
     return ans;
 }   
 
-async function connectSQL() {
 
-    client.connect()
-    var test = await client.query('SELECT * FROM japanese, svenska, espanol;')
-    console.log(test.rows[0])
-
-    var temp = await client.query('SELECT * FROM svenska')
-    console.log(temp.rows[0])
-
-}
 
