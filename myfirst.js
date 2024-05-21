2feb2e603011a8925463230f99a1adebbe89727b
Benjamin Came <bencame@iinet.net.au>
2024-05-21 18:35:06 +0000

    Working get requests

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,7 @@
 
 var pg = require('pg')
 var http = require('http');
+var cors = require('cors')
 const { error } = require('console');
 const CONTENT_TYPE_JSON = { "Content-Type": "application/json" };
 const { Pool } = pg
@@ -8,18 +9,26 @@ const { Pool } = pg
 const pool = new Pool ({
     host: 'localhost', 
     port: 5432,
-    username: 'polygot',
-    database: 'babel'
+    user: 'postgres',
+    database: 'babeldb',
+    password: 'pa$$word'
 })
 
 const bensServer = http.createServer((request, res) => {
     const { headers, method, url } = request;
+    const resheaders = {
+        'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Max-Age': 2592000, // 30 days
+        /** add other headers as per requirement */
+      };
 
     if (method === 'GET'){
         handelGetRequests(url).then(response => {
             console.log("this is my response   " + response)
-            console.log(JSON.stringify(response))
-            res.write(JSON.stringify(response))
+            console.log(JSON.stringify(response.rows))
+            res.writeHead(200, resheaders);
+            res.write(JSON.stringify(response.rows))
             res.end()
         })
         .catch(error => {
@@ -38,15 +47,15 @@ async function handelGetRequests (parsedURL) {
     if (parsedURL === "/japanese"){
         console.log("successgully passed handel request")
         temp = await getTable("japanese")
-        return temp.rows[0]
+	return temp
     } else if(parsedURL === "/svenska"){
         console.log("successgully passed handel request")
         temp = await getTable("svenska")
-        return temp.rows[0]
+        return temp
     } else if(parsedURL === "/espanol"){
         console.log("successgully passed handel request")
         temp = await getTable("espanol")
-        return temp.rows[0]
+        return temp
     } else {
         console.log("nothing to see here!!")
     }
@@ -56,8 +65,9 @@ async function getTable(table){
     console.log("get table")
     client = await pool.connect()
     var ans = await client.query('SELECT * FROM ' + table)
+    console.log("after")
     client.release()
-    console.log(ans.rows[0])
+    console.log("this is ans", ans)
     return ans;
 }   
 
