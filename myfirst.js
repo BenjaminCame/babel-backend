efd292a96978c0e40ab741b97954979f15ac43a5
Benjamin Came <bencame@iinet.net.au>
2024-05-30 14:59:19 +0000

    Querys working

--- myfirst.js
+++ myfirst.js
@@ -22,7 +22,7 @@ const bensServer = http.createServer((request, res) => {
         'Access-Control-Max-Age': 2592000, // 30 days
         /** add other headers as per requirement */
       };
-
+    //TODO need to add a (method === POST request)
     if (method === 'GET'){
         handelGetRequests(url).then(response => {
             console.log("this is my response   " + response)
@@ -45,17 +45,18 @@ bensServer.listen(8080, ()=>console.log("listening on 8080"))
 async function handelGetRequests (parsedURL) {
     //TODO this is working in the current form, but how can i dynamically add a table from the application
     if (parsedURL === "/addtable"){
-        temp = await createTable()
-        return temp
+        await createTable("testnew") //TODO need to return a success/failure repsonse
+        return
     }
 
     if (parsedURL === "/addphrase"){
-        temp = await createPhrase()
+        temp = await createPhrase("testnew", "native test", "target test") //TODO need to return a success/failure repsonse
+        return
     }
 
     if (parsedURL === "/japanese"){
         temp = await getTable("japanese")
-	return temp
+	    return temp
     } else if(parsedURL === "/svenska"){
         temp = await getTable("svenska")
         return temp
@@ -68,26 +69,23 @@ async function handelGetRequests (parsedURL) {
 }
 
 async function getTable(table){
-    console.log("get table")
     client = await pool.connect()
     var ans = await client.query('SELECT * FROM ' + table)
-    console.log("after")
     client.release()
-    console.log("this is ans", ans)
     return ans;
 }   
 
 async function createTable(tablename){
     client = await pool.connect()
-    console.log("NODE js createTable")
-    await client.query('CREATE TABLE' + tablename)//this is psudo sql query 
+    res = await client.query('CREATE TABLE ' + tablename +  "( native varchar(255), target varchar(255) );")
+    console.log(res)
     client.release()
 }
 
 async function createPhrase(table, native, target){
     client = await pool.connect()
-    console.log("adding" + native + target + " to " + tabel)
-    await client.query("INSERT INTO" + tablename + " VALUES (" + natve + "," + target + ")")
+    console.log("INSERT INTO " + table + " VALUES ('" + native + "','" + target + "');")
+    await client.query("INSERT INTO " + table + " VALUES ('" + native + "','" + target + "');")
     client.release()
 }
 
